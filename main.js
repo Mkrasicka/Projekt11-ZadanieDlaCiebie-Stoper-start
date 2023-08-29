e79c171e155d77a86002d454f47665885e88a2bb
magdalena krasicka <krasickamagdalena@gmail.com>
2023-08-29 17:56:13 +0100

    stoper iplementation

---
+++ main.js
@@ -0,0 +1,38 @@
+// http://websamuraj.pl/examples/js/projekt11/
+
+const mainBtn = document.querySelector('button.main');
+const resetBtn = document.querySelector('button.reset');
+const timeDiv = document.querySelector('.time div')
+let isPaused = false;
+let time = 0;
+let idInterval
+const startTime = new Date().getTime();
+
+const timer = function() {
+  if (!isPaused) {
+    isPaused = !isPaused
+    mainBtn.innerHTML = "Pauza";
+    idInterval = setInterval(start, 50);
+  } else {
+    isPaused = !isPaused;
+
+
+  }
+}
+
+const start = () => {
+  time++
+  timeDiv.innerHTML = (time / 100).toFixed(2);
+}
+
+const reset = () => {
+  time = 0;
+  clearInterval(idInterval);
+  mainBtn.innerHTML = "Start";
+  isPaused = false;
+  timeDiv.innerHTML = "---";
+}
+
+mainBtn.addEventListener('click', timer);
+
+resetBtn.addEventListener('click', reset)
